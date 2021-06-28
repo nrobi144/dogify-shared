297fb00ce14c8eeed930be1d6162c3da242d393a
nrobi144 <nagyrobi144@gmail.com>
2021-06-28 15:12:50 +0300

    Initialise repo

---
+++ build.gradle.kts
@@ -0,0 +1,72 @@
+import org.jetbrains.kotlin.gradle.plugin.mpp.KotlinNativeTarget
+
+buildscript {
+    repositories {
+        google()
+        mavenCentral()
+    }
+    dependencies {
+        classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:1.5.20")
+        classpath("com.android.tools.build:gradle:4.2.1")
+    }
+}
+
+plugins {
+    kotlin("multiplatform") version "1.5.20"
+    kotlin("native.cocoapods") version "1.5.20"
+    id("com.android.library") version "4.2.1"
+}
+
+version = "1.0"
+
+repositories {
+    google()
+    mavenCentral()
+}
+
+kotlin {
+    android()
+
+    val iosTarget: (String, org.jetbrains.kotlin.gradle.plugin.mpp.KotlinNativeTarget.() -> Unit) -> org.jetbrains.kotlin.gradle.plugin.mpp.KotlinNativeTarget =
+        if (System.getenv("SDK_NAME")?.startsWith("iphoneos") == true)
+            ::iosArm64
+        else
+            ::iosX64
+
+    iosTarget("ios") {}
+
+    cocoapods {
+        summary = "Dogify Shared module"
+        homepage = "https://github.com/nrobi144/dogify-shared"
+        ios.deploymentTarget = "14.1"
+        frameworkName = "shared"
+    }
+
+    sourceSets {
+        val commonMain by getting
+        val commonTest by getting {
+            dependencies {
+                implementation(kotlin("test-common"))
+                implementation(kotlin("test-annotations-common"))
+            }
+        }
+        val androidMain by getting
+        val androidTest by getting {
+            dependencies {
+                implementation(kotlin("test-junit"))
+                implementation("junit:junit:4.13.2")
+            }
+        }
+        val iosMain by getting
+        val iosTest by getting
+    }
+}
+
+android {
+    compileSdkVersion(30)
+    sourceSets["main"].manifest.srcFile("src/androidMain/AndroidManifest.xml")
+    defaultConfig {
+        minSdkVersion(23)
+        targetSdkVersion(30)
+    }
+}
---
+++ settings.gradle.kts
@@ -0,0 +1,11 @@
+pluginManagement {
+    repositories {
+        google()
+        gradlePluginPortal()
+        mavenCentral()
+    }
+}
+
+rootProject.name = "Dogify_Shared"
+include(":androidApp")
+include(":shared")
